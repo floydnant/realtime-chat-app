c5087f68892e9ff5503eac03650fe21deec5ae92
Floyd Haremsa <floyd.haremsa@berlin-bytes.de>
2022-07-09 23:19:04 +0200

    Update server base url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    // SERVER_BASE_URL: 'https://floyds-messenger-server.herokuapp.com',
-    SERVER_BASE_URL: 'https://realtime-chat-app.up.railway.app',
+    SERVER_BASE_URL: 'https://floyds-messenger-server.herokuapp.com',
+    // SERVER_BASE_URL: 'https://realtime-chat-app.up.railway.app',
 };
