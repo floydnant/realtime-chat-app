floydnant/realtime-chat-app

commit 38d82ecaae725f0b36a681ce42a1f8cb94f7add1

Refactor online/offline feature (#37)
* Refactor online/offline feature
* Randomly switch to tailwind in some places, Improve coloring of user status

server/src/socket/socket-manager.service.ts
@@ -1,21 +1,17 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Subject } from 'rxjs';
+import { Socket } from 'socket.io';
 import { IS_PROD } from 'src/constants';
 import { SocketEventPayloadMap } from 'src/shared/event-payload-map.model';
 import { UserPreview } from 'src/shared/index.model';
+import { SocketEvents } from 'src/shared/socket-events.model';
 import { UsersService } from 'src/users/users.service';
 import { TypedSocket } from './socket.gateway';
-
-export interface AuthenticatedUser {
-    username: string;
-    userId: string;
-    client: TypedSocket;
-}
 
 interface SocketQueue {
     eventName: keyof SocketEventPayloadMap;
     payload: SocketEventPayloadMap[keyof SocketEventPayloadMap];
-    clientId?: string;
+    roomId?: string | string[];
 }
 
 @Injectable()
@@ -35,107 +31,200 @@ export class SocketManagerService {
         eventName: E;
         payload: SocketEventPayloadMap[E];
         userId?: string;
+        roomId?: string | string[];
     }) {
-        const user = this.getUserOnline(userId, 'userId');
-        this.socketQueue.next({ ...event, clientId: user?.client?.id });
+        if (!userId) {
+            this.socketQueue.next(event);
+            return;
+        }
+
+        const { clients } = this.usersOnline.get(userId);
+        if (event.roomId) clients.values()[0].broadcast.to(event.roomId).emit(event.eventName, event.payload);
+        else clients.forEach(client => (client as Socket).emit(event.eventName, event.payload));
     }
 
-    private usersOnline: AuthenticatedUser[] = [];
-    private usersOnlineByChat: {
-        [chatId: string]: AuthenticatedUser[];
-    } = {};
-    private filterOnlineUsers(cb: (user: AuthenticatedUser) => boolean) {
-        Object.keys(this.usersOnlineByChat).forEach(chatId => {
-            this.usersOnlineByChat[chatId] = this.usersOnlineByChat[chatId].filter(cb);
-        });
-        this.usersOnline = this.usersOnline.filter(cb);
-    }
-    private filterOutDisconnectedClients(connectedClientIds: Set<string>) {
-        this.filterOnlineUsers(user => connectedClientIds.has(user.client.id));
-    }
+    private chatUsersMap = new Map</* chat id  */ string, /* user ids */ Set<string>>();
+    private usersOnline = new Map<string, { clients: Map<string, TypedSocket>; username: string }>();
+
     setUserOffline(clientId: string) {
-        this.filterOnlineUsers(u => u.client.id != clientId);
-    }
-    setUserOnline(user: AuthenticatedUser, chatIds: string[]) {
-        chatIds.forEach(chatId => this.addUserOnlineToChat(user, chatId, true));
-        this.usersOnline.push(user);
-    }
-    /* private */ addUserOnlineToChat(user: AuthenticatedUser, chatId: string, alreadyOnline = false) {
-        this.logger.verbose(`${user.username} was added to ${chatId}`);
-        user.client.join(chatId);
-        this.usersOnlineByChat[chatId] = [...(this.usersOnlineByChat[chatId] || []), user];
-        if (!alreadyOnline) this.usersOnline.push(user);
-    }
-    /* private */ removeUserOnlineFromChat(userId: string, chatId: string) {
-        this.usersOnlineByChat[chatId] = this.usersOnlineByChat[chatId].filter(u => u.userId != userId);
-    }
-    removeChatsWithoutUsersOnline() {
-        const newUsersOnlineByChat = {};
-        Object.entries(this.usersOnlineByChat).forEach(([chatId, users]) => {
-            if (users.length) newUsersOnlineByChat[chatId] = users;
-        });
-        this.usersOnlineByChat = newUsersOnlineByChat;
-    }
-    getUsersOnlineForChat(chatId: string) {
-        return this.usersOnlineByChat[chatId] || [];
-    }
-    getUserOnline(id: string, property?: keyof AuthenticatedUser): AuthenticatedUser | null {
-        property ||= 'client';
-        // const user = Object.values(this.usersOnlineByChat).reduce((prev: AuthenticatedUser | null, curr) => {
-        //     if (prev) return prev;
-        //     return curr.find(user => (property == 'client' ? user.client.id : user[property]) == id);
-        // }, null);
-        const user = this.usersOnline.find(user => (property == 'client' ? user.client.id : user[property]) == id);
-        if (!user) this.logger.warn(`Could not find user with ${property} '${id}'`);
+        this.usersOnline.forEach(({ username, clients }, userId) => {
+            const client = clients.get(clientId);
+            if (!client) return;
 
-        return user;
+            const chatIds = this.getUsersChatIds(userId);
+
+            this.chatUsersMap.forEach((usersSet, chatId) => {
+                if (!usersSet.has(userId)) return;
+
+                client.leave(chatId);
+                if (clients.size > 1) return;
+
+                // if only one user is online in the chat
+                if (usersSet.size == 1) {
+                    this.chatUsersMap.delete(chatId); // remove the whole chat
+                    return;
+                }
+                usersSet.delete(userId); // otherwise just remove the user
+
+                // emit stop typing (in case user was typing)
+                this.addToSocketQueue({
+                    roomId: chatId,
+                    eventName: SocketEvents.SERVER__TYPING_EVENT,
+                    payload: {
+                        username,
+                        isTyping: false,
+                        chatId,
+                    },
+                });
+            });
+
+            // if multiple clients are logged in with the same user
+            if (clients.size > 1) {
+                clients.delete(clientId); // just remove the one
+                return;
+            }
+            this.usersOnline.delete(userId); // otherwise remove the whole entry
+
+            // and emit status offline
+            this.addToSocketQueue({
+                roomId: chatIds,
+                eventName: SocketEvents.SERVER__USER_ONLINE_STATUS_EVENT,
+                payload: {
+                    user: { id: userId, username },
+                    chatIds: chatIds,
+                    online: false,
+                },
+            });
+        });
+        this.logUsersOnline();
     }
-    getChatsWithUser(clientId: string) {
-        return Object.entries(this.usersOnlineByChat)
-            .filter(([, users]) => users.some(u => u.client.id == clientId))
+
+    setUserOnline({
+        userId,
+        client,
+        username,
+        chatIds,
+    }: {
+        userId: string;
+        client: TypedSocket;
+        username: string;
+        chatIds: string[];
+    }) {
+        const user = this.usersOnline.get(userId);
+        if (user) {
+            user.clients.set(client.id, client);
+            this.logger.verbose(`'${user.username}' went online with another device.`);
+        } else {
+            this.logger.verbose(`'${username}' went online.`);
+            const clientsMap = new Map<string, TypedSocket>();
+            clientsMap.set(client.id, client);
+            this.usersOnline.set(userId, { username, clients: clientsMap });
+        }
+        chatIds.forEach(chatId => this.addUserToRoom(userId, chatId, client.id));
+        this.logUsersOnline();
+    }
+
+    getUsersChatIds(userId: string) {
+        return [...this.chatUsersMap.entries()]
+            .filter(([, usersSet]) => usersSet.has(userId))
             .map(([chatId]) => chatId);
     }
+
+    joinRoom(userId: string, chatId: string) {
+        this.addUserToRoom(userId, chatId);
+        this.logUsersOnline();
+    }
+    private addUserToRoom(userId: string, chatId: string, clientId?: string) {
+        const user = this.usersOnline.get(userId);
+        this.logger.verbose(`'${user.username}' was added to ${chatId}`);
+
+        const usersSet = this.chatUsersMap.get(chatId);
+        if (usersSet) usersSet.add(userId);
+        else this.chatUsersMap.set(chatId, new Set([userId]));
+
+        if (clientId) {
+            const client = user.clients.get(clientId);
+            client.join(chatId);
+            if (user.clients.size == 1)
+                // if a clientId is given, we can assume that it's an online event
+                client.broadcast.to(chatId).emit(SocketEvents.SERVER__USER_ONLINE_STATUS_EVENT, {
+                    chatIds: [chatId],
+                    online: true,
+                    user: { id: userId, username: user.username },
+                });
+        } else {
+            user.clients.forEach(client => client.join(chatId));
+            this.addToSocketQueue({
+                roomId: chatId,
+                eventName: SocketEvents.SERVER__USER_JOINED_CHAT,
+                payload: { chatId, user: { id: userId, username: user.username } },
+            });
+        }
+        this.addToSocketQueue({
+            // userId, // keep this outcommented until SERVER__USER_JOINED_CHAT sets users online on the client
+            eventName: SocketEvents.SERVER__USERS_ONLINE,
+            payload: { chatId, usersOnline: usersSet ? [...usersSet.values()] : [userId] },
+        });
+    }
+    leaveRoom(userId: string, chatId: string) {
+        const user = this.usersOnline.get(userId);
+        user.clients.forEach(client => client.leave(chatId));
+
+        const usersSet = this.chatUsersMap.get(chatId);
+        usersSet.delete(userId);
+        if (usersSet.size == 0) this.chatUsersMap.delete(chatId);
+        // @TODO: inform users here (idealy from the server to the show message only on all other users' devices)
+        // this.addToSocketQueue({
+        //     eventName: SocketEvents.
+        //     roomId: chatId
+        // })
+    }
+
+    getUserOnline(id: string, propertyName: 'userId' | 'clientId' = 'clientId') {
+        let userPreview: UserPreview;
+        if (propertyName == 'clientId') {
+            const [id, { username }] = [...this.usersOnline.entries()].find(([, { clients }]) => clients.has(id));
+            userPreview = { id, username };
+        } else {
+            const { username } = this.usersOnline.get(id);
+            userPreview = { id, username };
+        }
+
+        if (!userPreview) this.logger.warn(`Could not find user with ${propertyName} '${id}'`);
+
+        return userPreview;
+    }
+
+    // @TODO: replace this
     async logUsersOnline() {
         if (IS_PROD) return;
-        const usersOnlinePerChat = await Promise.all(
-            Object.entries(this.usersOnlineByChat).map(async ([chatId, users]) => ({
-                // ...(await this.chatService.getChatName(chatId)),
-                title: 'no title',
+
+        const data = [...this.chatUsersMap.entries()].map(([chatId, usersSet]) => {
+            const users = [...usersSet.values()].map(id => this.usersOnline.get(id).username);
+            return {
                 chatId,
-                users: users.map(u => u.username),
-            })),
-        );
-        const usersOnlineMap = {};
-        usersOnlinePerChat.forEach(({ chatId, title, users }) => {
-            // usersOnlineMap[`${title} -- ${chatId}`] = users;
-            usersOnlineMap[chatId] = { title, users };
+                users,
+            };
         });
-        // console.log('users online per chat:', usersOnlineMap, '\n');
-        console.table(usersOnlineMap);
+        console.table(data);
+        // const usersOnlinePerChat = await Promise.all(
+        //     Object.entries(this.usersOnlineByChat).map(async ([chatId, users]) => ({
+        //         // ...(await this.chatService.getChatName(chatId)),
+        //         title: 'no title',
+        //         chatId,
+        //         users: users.map(u => u.username),
+        //     })),
+        // );
+        // const usersOnlineMap = {};
+        // usersOnlinePerChat.forEach(({ chatId, title, users }) => {
+        //     // usersOnlineMap[`${title} -- ${chatId}`] = users;
+        //     usersOnlineMap[chatId] = { title, users };
+        // });
+        // // console.log('users online per chat:', usersOnlineMap, '\n');
+        // console.table(usersOnlineMap);
     }
 
-    onClientConnected(connectedClientIds: Set<string>) {
-        this.filterOutDisconnectedClients(connectedClientIds);
-    }
-
-    onClientLogout(clientId: string): { loggedOutUser: UserPreview | null; chatIds: string[] } | undefined {
-        const loggedOutUser = this.getUserOnline(clientId);
-        if (!loggedOutUser) return;
-
-        const chatIds = this.getChatsWithUser(clientId);
-        this.setUserOffline(clientId);
-
-        this.removeChatsWithoutUsersOnline();
-        this.logUsersOnline();
-
-        const { userId: id, username } = loggedOutUser;
-        return {
-            loggedOutUser: { username, id },
-            chatIds,
-        };
-    }
-
-    async authenticateSocket(clientId: string, accessToken: string) {
+    async authenticateSocket(accessToken: string) {
         const { authenticated, id, username, chatIds } = await this.usersService.authenticateFromToken(accessToken);
 
         return {

client/src/app/app.component.html
@@ -1,3 +1,3 @@
-<main class="h-full">
+<main class="h-full w-full max-w-[90rem] mx-auto">
     <router-outlet #outlet="outlet"></router-outlet>
 </main>

client/src/app/app.component.scss
@@ -1,5 +1,0 @@
-main {
-    width: 100%;
-    max-width: 90rem;
-    margin: 0 auto;
-}

client/src/app/pages/chat/chat.component.html
@@ -1,18 +1,19 @@
 <sidebar></sidebar>
 <div class="_container">
-    <div class="chat-title-bar">
+    <div class="chat-title-bar | w-full p-3 z-10">
         <h1 class="chat-title | !mb-0 text-2xl">{{ activeChat?.title }}</h1>
+        <!-- @FIXME: truncating (container is expanding instead of limiting) -->
         <div
-            class="users-online cut-text"
-            [class.show]="usersOnlineText$ | async"
-            [innerHTML]="usersOnlineText$ | async"
+            class="users-online w-full text-gray-500"
+            [class.show]="activeChat?.onlineStatus"
+            [innerHTML]="activeChat?.onlineStatus"
         ></div>
     </div>
     <div class="chat" #chatRef>
         <div *ngIf="!chatsState.loadingActiveChatMessages" class="messages">
             <div *ngFor="let message of messages; let i = index">
                 <chat-message
-                    *ngIf="!isSameDay(message.timestamp, messages[i - 1]?.timestamp || '')"
+                    *ngIf="!isSameDay(message.timestamp, messages[i - 1]?.timestamp || '')" 
                     [variant]="MessageTypes.USER_EVENT"
                     [messageText]="getDay(message.timestamp) || 'Invalid date'"
                 ></chat-message>

client/src/app/pages/chat/chat.component.scss
@@ -1,32 +1,22 @@
 :host {
-    height: 100vh;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
+    @apply h-screen w-full max-w-full
+        flex
+        justify-center
+        items-center;
 }
 ._container {
     box-sizing: border-box;
     position: relative;
-    margin: 0 auto;
-    height: calc(100% - 0px);
-    // width: min(100%, 800px);
-    @apply w-full;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
+    @apply h-full 
+        w-full 
+        flex
+        flex-col
+        justify-between
+        items-center
+        mx-auto;
 }
 .chat-title-bar {
-    @apply w-full p-3 z-10 /* bg-fade-to-bottom */;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-
-
     .users-online {
         font-size: 0.8rem;
 
@@ -114,7 +104,6 @@
     &.visible {
         padding-top: 15px;
         margin-bottom: 10px;
-
 
         .dots {
             opacity: 1;

client/src/app/pages/chat/chat.component.ts
@@ -45,7 +45,24 @@ export class ChatComponent implements OnDestroy, AfterViewInit {
             this.user = user.loggedInUser;
             this.chatsState = chats;
         });
-        this.store.select(chatsSelectors.selectActiveChat).subscribe(chat => (this.activeChat = chat));
+        this.store.select(chatsSelectors.selectActiveChat).subscribe(chat => {
+            const membersOnlineStatus = chat?.members
+                ?.sort((a, b) => Number(b.isOnline) - Number(a.isOnline))
+                .map(
+                    m =>
+                        `<span class="${m.isOnline ? 'text-primary-400' : ''}">${
+                            m.id == this.user?.id ? 'You' : m.username
+                        }</span>`,
+                )
+                .join(', ');
+            const friendOnlineStatus =
+                chat?.isOnline === false ? 'offline' : chat?.isOnline && '<span class="text-primary-400">online</span>';
+
+            this.activeChat = chat && {
+                ...chat,
+                onlineStatus: membersOnlineStatus || friendOnlineStatus,
+            };
+        });
     }
 
     ngOnDestroy() {
@@ -54,7 +71,7 @@ export class ChatComponent implements OnDestroy, AfterViewInit {
 
     chatsState: ChatsState;
     user: LoggedInUser | null;
-    activeChat: ChatPreview | null;
+    activeChat: (ChatPreview & { onlineStatus: string | undefined }) | null;
 
     MessageTypes = MessageTypes;
     ChatType = ChatType;
@@ -127,18 +144,6 @@ export class ChatComponent implements OnDestroy, AfterViewInit {
                 .join(', ')} ${usersTyping.length > 1 ? 'are' : 'is'} typing`;
 
             return usersTypingText;
-        }),
-    );
-
-    usersOnlineText$ = this.chatService.getUsersOnline().pipe(
-        map(usersOnline => {
-            const users = (usersOnline || []).filter(u => u != this.user?.username);
-            if (this.activeChat?.chatType == ChatType.PRIVATE)
-                return users.length > 1
-                    ? '<span class="text-primary-400">online</span>'
-                    : '<span class="text-gray-300">offline</span>';
-
-            return users.map(u => `<span class="text-primary-400">${escapeHTML(u)}</span>`).join(', ');
         }),
     );
 

client/src/app/services/chat.service.ts
@@ -4,7 +4,7 @@ import { Store } from '@ngrx/store';
 import { Subject } from 'rxjs';
 import { filter, map, tap } from 'rxjs/operators';
 import { Client_ChatMessagePayload } from 'src/shared/chat-event-payloads.model';
-import { ChatType } from 'src/shared/index.model';
+import { ChatGroup, ChatType, FriendshipData } from 'src/shared/index.model';
 import { SocketEvents } from 'src/shared/socket-events.model';
 import { AppState } from '../store/app.reducer';
 import { chatActions } from '../store/chat/chat.actions';
@@ -66,25 +66,18 @@ export class ChatService {
         .subscribe(({ chatId }) => this.usersTypingForActiveChat.next(this.usersTypingMap[chatId] || []));
 
     // users online
-    private usersOnlineForActiveChat = new Subject<string[]>();
-    getUsersOnline() {
-        return this.usersOnlineForActiveChat.asObservable().pipe(map(usersOnline => ['You', ...usersOnline]));
-    }
-    private usersOnlineMap: { [chatId: string]: string[] } = {};
     private usersOnlineEvents = this.socket
         .fromEvent(SocketEvents.SERVER__USERS_ONLINE)
-        .pipe(
-            tap(({ chatId, usersOnline }) => {
-                this.usersOnlineMap[chatId] = usersOnline;
-            }),
-            filter(({ chatId }) => chatId == this.chatState.activeChatId),
-        )
-        .subscribe(({ usersOnline }) => this.usersOnlineForActiveChat.next(usersOnline));
+        .subscribe(({ usersOnline }) => {
+            // @TODO: handle this with one action instead of dipatching the same one multiple times
+            usersOnline.forEach(id => {
+                this.store.dispatch(chatActions.setUserOnlineStatus({ userId: id, isOnline: true }));
+            });
+        });
 
     private setActiveChatEvents = this.actions$
         .pipe(ofType(chatActions.setActiveChatSuccess))
         .subscribe(({ chatId }) => {
-            this.usersOnlineForActiveChat.next(this.usersOnlineMap[chatId] || []);
             this.usersTypingForActiveChat.next(this.usersTypingMap[chatId] || []);
         });
 
@@ -110,9 +103,12 @@ export class ChatService {
 
     // this is later also gonna contain join, leave, etc. events
     getUserEvents() {
-        return this.socket
-            .fromEvent(SocketEvents.SERVER__USER_ONLINE_STATUS_EVENT)
-            .pipe(filter(({ chatIds }) => chatIds.some(id => id == this.chatState.activeChatId)));
+        return this.socket.fromEvent(SocketEvents.SERVER__USER_ONLINE_STATUS_EVENT).pipe(
+            tap(({ user, online }) => {
+                this.store.dispatch(chatActions.setUserOnlineStatus({ userId: user.id, isOnline: online }));
+            }),
+            filter(({ chatIds }) => chatIds.some(id => id == this.chatState.activeChatId)),
+        );
     }
 
     // CRUD stuff
@@ -121,5 +117,11 @@ export class ChatService {
             chatType == 'group' ? `/chats/chat/${chatId}/messages` : `/friendships/${chatId}/messages`,
         );
         return messages;
+    }
+
+    getChatData({ chatId, chatType }: { chatId: string; chatType: ChatType }) {
+        return this.httpClient.get<ChatGroup | FriendshipData>(
+            chatType == 'group' ? `/chats/chat/${chatId}/data` : `/friendships/${chatId}/data`,
+        );
     }
 }

client/src/app/store/chat/chat-group.effects.ts
@@ -4,6 +4,8 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { mergeMap, map } from 'rxjs';
 import { ChatGroupService } from 'src/app/services/chat-group.service';
 import { FriendshipService } from 'src/app/services/friendship.service';
+import { SocketService } from 'src/app/services/socket.service';
+import { SocketEvents } from 'src/shared/socket-events.model';
 import { throwIfErrorExists, catchAndHandleError, handleResponse } from '../app.effects';
 import { chatActions } from './chat.actions';
 
@@ -14,6 +16,7 @@ export class ChatGroupEffects {
         private chatGroupService: ChatGroupService,
         private friendshipService: FriendshipService,
         private toastService: HotToastService,
+        private socket: SocketService,
     ) {}
 
     createChat = createEffect(() => {
@@ -70,4 +73,10 @@ export class ChatGroupEffects {
             ),
         );
     });
+
+    addJoinedMemberToGroup = createEffect(() =>
+        this.socket
+            .fromEvent(SocketEvents.SERVER__USER_JOINED_CHAT)
+            .pipe(map(({ chatId, user }) => chatActions.addMemberToGroup({ chatId, newMember: user }))),
+    );
 }

commit f1fc3aab7ef852dc5e93c5fe7f9c75cbe139fcfd

Fix error introduced by #37

server/src/socket/socket-manager.service.ts
@@ -185,8 +185,8 @@ export class SocketManagerService {
     getUserOnline(id: string, propertyName: 'userId' | 'clientId' = 'clientId') {
         let userPreview: UserPreview;
         if (propertyName == 'clientId') {
-            const [id, { username }] = [...this.usersOnline.entries()].find(([, { clients }]) => clients.has(id));
-            userPreview = { id, username };
+            const [userId, { username }] = [...this.usersOnline.entries()].find(([, { clients }]) => clients.has(id));
+            userPreview = { id: userId, username };
         } else {
             const { username } = this.usersOnline.get(id);
             userPreview = { id, username };